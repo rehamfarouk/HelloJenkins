cfa8efcd3172e1d817c74507a3d193aa241d176f
rehamfarouk <rehamfarouk91@yahoo.com>
2019-02-23 10:47:54 -0800

    sec program

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ class HelloWorld {
    
     public static void main(String args[]) { 
           System.out.println("Hello World");
-          int c=78;
-          System.out.println(c);
+          int c=100;
+          int d=90;
+          System.out.println("Sum:"+c+d);
      }
 }
